4bbfd04fbb90a10eaf56dd19b4b04329480c95a9
Ryan <re.mcclue@gmail.com>
2018-08-08 13:01:47 +1000

    no triggering file uploads

---
+++ file-reading.js
@@ -0,0 +1,10 @@
+// lighten/darken_color() 
+
+function load_is_clicked() {
+  let file_selector_elem = document.createElement("input");	
+  file_selector_elem.type = "file";
+  file_selector_elem.style = "opacity: 0";
+
+  file_selector_elem.click();
+}
+
