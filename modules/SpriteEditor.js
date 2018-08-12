87ce462a46c4b5de9f343e94ed9ae77b497f9a83
Ryan <re.mcclue@gmail.com>
2018-08-13 09:28:35 +1000

    as

--- modules/SpriteEditor.js
+++ modules/SpriteEditor.js
@@ -1,81 +1,13 @@
 import TxtEngine from "./lib/TxtEngine.js";
 import * as Utils from "./Utils.js";
 
-class Label {
-  constructor(x, y, txt, bg_color, fg_color) {
-    this.x = x;
-    this.y = y;
-    this.txt = txt;
-    this.bg_color = bg_color;
-    this.fg_color = fg_color;
-  }
-  
-  update_and_render() {
-    this.render_str(); 
-  }
-}
-
-class VariableLabel extends Label {
-        // must be arr to be passed by reference
-  constructor(x, y, txt, bg_color, fg_color, variable_arr) {
-    super(x, y, txt, bg_color, fg_color);
-    this.variable_arr = variable_arr;
-  }
-
-  update() {
-    let output_str = `${this.txt}: ${this.variable_arr[0]}`;
-    this.render_str();
-  }
-}
-
-class InputLabel extends Label {
-  constructor(x, y, txt, bg_color, fg_color, max_width) {
-    super(x, y, txt, bg_color, fg_color);
-    this.content = new Array(max_width);
-  }
-
-  get content() {
-    return this.content.join(""); 
-  }
-
-  update() {
-    if (this.input.x = )
-    let output_str = `${this.txt}: ${this.content.join("")}`;
-    this.render_str();
-  }
-  
-}
-
-class TouchableLabel extends Label {
-  constructor(x, y, txt, bg_color, fg_color, max_width) {
-    super(x, y, txt, bg_color, fg_color);
-    this.is_active = false;
-  }
-
-  update() {
-    if (!this.is_active) {
-      if (this.input.pointer.has_touched && this.input.pointer.x >= this.x && this.input.pointer.x < this.txt.length + this.max_width && this.input.pointer.y === this.y) {
-        this.is_active = true; 
-      }
-    } else {
-       
-    }
-  }
-
-  // when has been deactivated
-  reset() {
-  
-  }
-}
-
-
-
-
 export default class SpriteEditor extends TxtEngine {
   constructor(canvas_dom_elem, width, height, default_bg, default_fg, default_ch) {
     super(canvas_dom_elem, width, height, default_bg, default_fg, default_ch);
 
-    this.active_tool = "move";
+    this.active_label = "move";
+	this.are_hovering_over_label = false;
+	this.recent_hovering_label = null;
 
     this.labels = {
 	  "move": this._create_touchable_label(3, 4, "MOVE", "blue", "white", () => {
@@ -89,75 +21,113 @@ export default class SpriteEditor extends TxtEngine {
 	            }
 	          }),
 	};
-
-    this.tools = {
-      "move":
-	}
-
-          // store sprites as .spr, but import as js file
-          // contains:
-          // let sprite_data = TypedArray(); 
-          // export { sprite_data as default };
-
-    let brush_label = create_label(3, 4, "MOVE"); 
-    let erase_label = create_label(3, 4, "MOVE"); 
-    let zoom_in_label = create_label(3, 4, "MOVE"); 
-    let zoom_out_label = create_label(3, 4, "MOVE"); 
-    let undo_label = create_label(3, 4, "MOVE"); 
-    let redo_label = create_label(3, 4, "MOVE"); 
-    let cut_label = create_label(3, 4, "MOVE"); 
-    let copy_label = create_label(3, 4, "MOVE"); 
-    let paste_label = create_label(3, 4, "MOVE"); 
-    let output_label = create_label(3, 4, "MOVE"); 
-    let load_label = create_label(3, 4, "MOVE"); 
-    
-    width, height, width_plus, width_minus, 
   }	
 
-  update(delta_time) {
-	this._render_base_gui();
-    
-	this.labels.forEach((label) => {
-          label.update_and_render();
-	});
-
-    this.tools[this.active_tool].process_input();
-      
-    this.shortcuts.forEach()
-  }
-
-  _render_base_gui() {
-    	  
-  }
-
-  _create_touchable_label(x, y, txt, bg_color, fg_color, touch_callback) {
-    let label = this._create_static_label(x, y, txt, bg_color, fg_color);
-	label.is_active = false;
-	label.handle_touch = () => {
-      if (this.input.pointer.has_touched) {
-		if (this.input.pointer.x >= label.x && this.input.pointer.x < label.txt.length &&
-		     this.input.pointer.y == label.y) {
-	      touch_callback();
-		}
-	  }
-	};
-  }
-
-  _create_static_label(x, y, txt, bg_color, fg_color) {
+  _create_label(x, y, txt, bg_color, fg_color) {
     let label = Object.create(null);
+    label.x = x;
+    label.y = y;
     label.txt = txt;
     label.bg_color = bg_color;
     label.fg_color = fg_color;
+	label.width = txt.length;
 
-    // arrow functions necessary to preserve 'this'
-    label.handle_hover = () => {
-	  if (this.input.pointer.x >= label.x && this.input.pointer.x < label.txt.length &&
-		this.input.pointer.y === label.y) {
-	     this.render_str(label.x, label.y, Utils.lighten(label.bg_color, 0.3), Utils.lighten(label.fg_color, 0.3), txt);
+    label.update = () => {
+	  this.render_str(x, y, txt, bg_color, fg_color);	
+    };
+  }
+
+  _is_hovering_over_label(label) {
+    return (this.input.pointer.x >= label.x && this.input.pointer.x < label.width &&
+	         this.input.pointer.y === label.y);
+  }
+
+  _has_clicked_label(label) {
+    return (this.input.pointer.has_touched && this._is_hovering_over_label(label));
+  }
+
+  _create_variable_label(x, y, txt, bg_color, fg_color, variable, max_width) {
+    let variable_label = this._create_label(x, y, txt, bg_color, fg_color);	  
+	variable_label.width = txt.length + max_width + 2;
+	variable_label.variable = variable;
+
+    variable_label.update = () => {
+	  this.render_str(x, y, `${txt}: ${variable.substr(0, max_width)}`, bg_color, fg_color);	
+    };
+  }
+
+  _create_input_label(x, y, txt, bg_color, fg_color, max_width) {
+    let input_label = this._create_label(x, y, txt, bg_color, fg_color);	  
+	input_label.content = new Array(max_width);
+	input_label.is_active = false;
+	input_label.width = txt.length + max_width + 2; // for ':<space>'
+
+    input_label.hover_reset(want_to_revert) {
+      if (!want_to_revert) {
+	    document.body.style.cursor = "text";
+	  } else 
+	}
+
+    input_label.update = () => {
+	  if (!input_label.is_active) {
+	    if (this._are_hovering_over_label(input_label)) {
+		  if (this.recent_hovering_label !== null) {
+		    this.labels[this.recent_hovering_label].hover_reset(true);
+		  }
+	      this.are_hovering_over_a_label = true; 
+		  this.recent_hovering_label = input_label;
+		  input_label.hover_reset(false);
+		}
+		if (this._has_clicked_label(input_label)) {
+		  input_label.hover_reset(true);
+
+	      let cursor_pos = 10; // alter
+		  this.labels[this.active_label].is_active = false;
+		  input_label.is_active = true;
+		}
+	  } else {
+	    // handle char input	  
 	  }
-	};
+	  this.render_str(x, y, txt, bg_color, fg_color);	
+    };
+  }
 
-    return label;
+  _create_touchable_label(x, y, txt, bg_color, fg_color, on_touch) {
+    let touchable_label = this._create_label(x, y, txt, bg_color, fg_color);	  
+	touchable_label.is_active;
+
+    touchable_label.update = () => {
+	  if (!touchable_label.is_active) {
+	    if (this._is_hovering_over_label(touchable_label)) {
+	      touchable_label.fg_color = Utils.lighten(touchable_label.fg_color, 0.3);	
+		} 
+	  }
+	  this.render_str(x, y, txt, bg_color, fg_color);	
+    };
+  }
+
+  _create_tool_label(x, y, txt, bg_color, fg_color, on_active) {
+    let input_label = this._create_label(x, y, txt, bg_color, fg_color);	  
+	let input_label.content = new Array(max_width);
+
+    variable_label.update = () => {
+	  this.render_str(x, y, txt, bg_color, fg_color);	
+    };
+	  
+  }
+
+  update(delta_time) {
+	this.are_hovering_over_a_label = false;
+
+    this.shortcuts.forEach()
+    
+	this.labels.forEach((label) => {
+      label.update_and_render();
+	});
+
+	if (!this.are_hovering_over_label && this.recent_hovering_label !== null) {
+	  this.hovering_label.reset_hover();
+	}
   }
 
 }
