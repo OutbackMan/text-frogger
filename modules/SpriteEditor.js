80cd8e85efdea29a6cc58b593b1c4eaccd29be3c
Ryan <re.mcclue@gmail.com>
2018-08-13 15:08:21 +1000

    more

--- modules/SpriteEditor.js
+++ modules/SpriteEditor.js
@@ -5,12 +5,12 @@ export default class SpriteEditor extends TxtEngine {
   constructor(canvas_dom_elem, width, height, default_bg, default_fg, default_ch) {
     super(canvas_dom_elem, width, height, default_bg, default_fg, default_ch);
 
-    this.active_label = null;
-	this.have_detected_hovering_label_this_frame = false;
-	this.recent_hovering_label = null;
+    this.most_recently_touched_label = null;
+	this.have_detected_a_hovered_label_this_frame = false;
+	this.active_hovered_label = null;
 
     this.labels = {
-	  "move": this._create_touchable_label()
+	  "move": this._create_var_label(this.editable_sprite.cursor_pos)
 	};
   }	
 
@@ -26,17 +26,17 @@ export default class SpriteEditor extends TxtEngine {
 	 *			   As a result, don't put labels right next to each other. Leave
      *			   some padding.
 	 */ 
-	this.have_detected_hovering_label_this_frame = false;
+	this.have_detected_a_hovered_label_this_frame = false;
 
     this._listen_for_keyboard_shortcuts();
     
 	this.labels.forEach((label) => {
-      label.update_and_render();
+      label.update();
 	});
 
-	if (!this.have_detected_hovering_label_this_frame && 
-		  this.recent_hovering_label !== null) {
-	  this.recent_hovering_label.reset_hover_props(true);
+	if (!this.have_detected_a_hovered_label_this_frame && 
+		  this.active_hovered_label !== null) {
+	  this.active_hovered_label.clear_hover_props();
 	}
   }
 
@@ -59,70 +59,61 @@ export default class SpriteEditor extends TxtEngine {
 	label.width = static_txt.length;
 
     label.update = () => {
-	  this.render_str(label.x, label.y, label.txt, label.bg_color, label.fg_color);	
+	  this.render_str(label.x, label.y, label.static_txt, label.bg_color, label.fg_color);	
     };
   }
 
-  _is_hovering_over_label(label) {
-    return (this.input.pointer.x >= label.x && this.input.pointer.x < label.width &&
-	         this.input.pointer.y === label.y);
-  }
 
-  _has_clicked_label(label) {
-    return (this.input.pointer.has_touched && this._is_hovering_over_label(label));
-  }
-
-  _create_var_label(x, y, txt, bg_color, fg_color, variable, var_width) {
-    let var_label = this._create_label(x, y, txt, bg_color, fg_color);	  
+  // advised to pass "static_txt :"
+  _create_var_label(x, y, static_txt, bg_color, fg_color, variable, var_width) {
+    let var_label = this._create_label(x, y, static_txt, bg_color, fg_color);	  
 	var_label.variable = variable;
-	var_label.width = var_label.txt.length + var_width + 2;
+	var_label.var_width = var_width;
+	var_label.width = var_label.static_txt.length + var_width;
 
     var_label.update = () => {
 	  this.render_str(
 	    var_label.x, 
 		var_label.y, 
-		`${var_label.txt}: ${var_label.variable.substr(0, var_label.var_width)}`, 
+		`${var_label.static_txt}${String(var_label).variable.substr(0, var_label.var_width)}`, 
 		var_label.bg_color, 
 		var_label.fg_color
 	  );	
     };
   }
 
-  _create_input_label(x, y, txt, bg_color, fg_color, input_width) {
-    let input_label = this._create_label(x, y, txt, bg_color, fg_color);	  
-	input_label.width = input_label.txt.length + input_width + 2;
+  _create_input_label(x, y, static_txt, bg_color, fg_color, input_width) {
+    let input_label = this._create_label(x, y, static_txt, bg_color, fg_color);	  
+	input_label.width = input_label.static_txt.length + input_width;
 	input_label.input_width = input_width;
 	input_label.content = new Array(input_width).fill(" ");
-	input_label.is_active = false;
+	input_label.has_been_touched = false;
 	input_label.content_cursor_pos = -1;
 	input_label.render_cursor_pos = -1;
 
-    input_label.apply_hover = () => {
+    input_label.apply_hover_props = () => {
+	  this.have_detected_a_hovered_label_this_frame = true; 
+	  this.active_hovered_label = input_label;
+
 	  document.body.style.cursor = "text";
-	  // lighten input_label.fg_color =
-	  this.are_hovering_over_a_label = true; 
-	  this.recent_hovering_label = input_label;
 	};
 
-    input_label.clear_hover = () => {
+    input_label.clear_hover_props = () => {
+	  this.active_hovered_label = null;
+
 	  document.body.style.cursor = "default";
-	  this.recent_hovering_label = null;
 	};
 
-    input_label.deactivate = () => {
-	  document.body.style.cursor = "default";
-	  input_label.is_active = false;
-	};
+    input_label.apply_touched_props = () => {
+	  this.most_recently_touched_label.clear_touched_props();
+	  input_label.has_been_touched = true;
+	  this.most_recently_touched_label = input_label;
 
-    input_label.activate = () => {
 	  document.body.style.cursor = "text";
-	  this.active_label.deactivate();
-	  input_label.is_active = true;
-	  this.active_label = input_label;
 
       let x_delta = this.input.pointer.x - input_label.x;
-	  if (x_delta < input_label.txt.length + 2) {
-        input_label.render_cursor_pos = input_label.x + input_label.txt.length + 2;
+	  if (x_delta < input_label.static_txt.length) {
+        input_label.render_cursor_pos = input_label.x + input_label.static_txt.length; 
 	    input_label.content_cursor_pos = 0; 
 	  } else {
 	    input_label.content_cursor_pos = x_delta;
@@ -130,16 +121,22 @@ export default class SpriteEditor extends TxtEngine {
 	  }
 	}
 
+    input_label.clear_touched_props = () => {
+	  input_label.has_been_touched = false;
+
+	  document.body.style.cursor = "default";
+	};
+
     input_label._inc_render_cursor_pos () => {
 	  if (input_label.render_cursor_pos + 1 === input_label.width) {
-	    input_label.render_cursor_pos = input_label.x + input_label.txt.length + 2; 	  
+	    input_label.render_cursor_pos = input_label.x + input_label.static_txt.length; 	  
 	  } else {
 	    input_label.render_cursor_pos++;
 	  }
 	}
 
     input_label._dec_render_cursor_pos = () => {
-	  if (input_label.render_cursor_pos - 1 < input_label.x + input_label.txt.length + 2) {
+	  if (input_label.render_cursor_pos - 1 < input_label.x + input_label.txt.length) {
 	    input_label.render_cursor_pos = input_label.x + input_label.width - 1;
 	  } else {
 	    input_label.render_cursor_pos--;
@@ -147,12 +144,12 @@ export default class SpriteEditor extends TxtEngine {
 	}
 
     input_label.update = () => {
-	  if (!input_label.is_active) {
+	  if (!input_label.has_been_touched) {
 	    if (this._are_hovering_over_label(input_label)) {
-		  input_label.apply_hover();
+		  input_label.apply_hover_props();
 		}
-		if (this._has_clicked_label(input_label)) {
-		  input_label.activate();
+		if (this._has_touched_label(input_label)) {
+		  input_label.apply_touched_props();
 	  } else {
 	    if (this.pointer.keys["ArrowRight"].is_pressed) {
 		  input_label._inc_render_cursor_pos();
@@ -177,16 +174,19 @@ export default class SpriteEditor extends TxtEngine {
 		    } 
 		  });
 		}
-	    this.render_ch(input_label.render_cursor_pos, input_label.y, " ", input_label.bg_color.darken, fg_color);
 	  }
 
 	  this.render_str(
 	    input_label.x, 
 		input_label.y, 
-		`${input_label.txt}: ${input_label.content}`,
+		`${input_label.static_txt}${input_label.content}`,
 		input_label.bg_color, 
 		input_label.fg_color
 	  );	
+
+	  if (input_label.has_been_touched) {
+	    this.render_ch(input_label.render_cursor_pos, input_label.y, " ", input_label.bg_color.darken, fg_color);
+	  }
     };
   }
 
@@ -212,6 +212,15 @@ export default class SpriteEditor extends TxtEngine {
 	  this.render_str(x, y, txt, bg_color, fg_color);	
     };
 	  
+  }
+
+  _is_hovering_over_label(label) {
+    return (this.input.pointer.x >= label.x && this.input.pointer.x < label.width &&
+	         this.input.pointer.y === label.y);
+  }
+
+  _has_clicked_label(label) {
+    return (this.input.pointer.has_touched && this._is_hovering_over_label(label));
   }
 
   }
